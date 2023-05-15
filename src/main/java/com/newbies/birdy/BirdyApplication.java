e9864942f60c52145fdd2c3dbbc816eb1fe35b9a
blvm159263 <builevanminh0809@gmail.com>
2023-05-15 12:52:34 +0700

    first commit

---
+++ src/main/java/com/newbies/birdy/BirdyApplication.java
@@ -0,0 +1,13 @@
+package com.newbies.birdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdyApplication.class, args);
+	}
+
+}
