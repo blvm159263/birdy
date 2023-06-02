70758a2284e508820e923a9e03e98e6cc7cf462b
blvm159263 <builevanminh0809@gmail.com>
2023-06-02 10:53:54 +0700

    update cors port

--- src/main/java/com/newbies/birdy/security/CORSConfiguration.java
+++ src/main/java/com/newbies/birdy/security/CORSConfiguration.java
@@ -11,7 +11,7 @@ public class CORSConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("https://birdy-fe.vercel.app/")
+                .allowedOrigins("http://localhost:3000/")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(-1);
