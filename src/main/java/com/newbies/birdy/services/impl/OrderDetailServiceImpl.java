a07bd6944e00b5e373cc64cf68276ef4b87d0e64
n-d-h <duchoang4195@gmail.com>
2023-06-23 22:38:23 +0700

    update api

--- src/main/java/com/newbies/birdy/services/impl/OrderDetailServiceImpl.java
+++ src/main/java/com/newbies/birdy/services/impl/OrderDetailServiceImpl.java
@@ -3,8 +3,10 @@ package com.newbies.birdy.services.impl;
 import com.newbies.birdy.dto.OrderDetailDTO;
 import com.newbies.birdy.entities.Order;
 import com.newbies.birdy.entities.OrderDetail;
+import com.newbies.birdy.exceptions.entity.EntityNotFoundException;
 import com.newbies.birdy.mapper.OrderDetailMapper;
 import com.newbies.birdy.repositories.OrderDetailRepository;
+import com.newbies.birdy.repositories.OrderRepository;
 import com.newbies.birdy.services.OrderDetailService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,12 +20,12 @@ import java.util.List;
 public class OrderDetailServiceImpl implements OrderDetailService {
 
     private final OrderDetailRepository orderDetailRepository;
+    private final OrderRepository orderRepository;
 
     @Override
     public List<OrderDetailDTO> getOrderDetailsByOrderIdAndStatus(Integer orderId, Boolean status) {
-        Order Order = new Order();
-        Order.setId(orderId);
-        List<OrderDetail> orderDetailList = orderDetailRepository.findByOrderAndStatus(Order, status);
+        Order order = orderRepository.findByIdAndStatus(orderId, status).orElseThrow(() -> new EntityNotFoundException("Order not found"));
+        List<OrderDetail> orderDetailList = orderDetailRepository.findByOrderAndStatus(order, status);
         return orderDetailList.stream().map(OrderDetailMapper.INSTANCE::toDTO).toList();
     }
 }
